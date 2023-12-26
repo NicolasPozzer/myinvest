47828dae05435ebe62f619b6eff99cf99e4fd259
NicolasPozzer <pozzernico@gmail.com>
2023-12-26 09:29:33 -0300

    fix gitignore

---
+++ src/main/java/com/demo/myinvest/MyinvestApplication.java
@@ -0,0 +1,13 @@
+package com.demo.myinvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyinvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyinvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/myinvest/MyinvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.myinvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
