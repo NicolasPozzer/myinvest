b646ab663daabf26ad97232f13984e7e7c37eab5
NicolasPozzer <pozzernico@gmail.com>
2023-12-27 07:23:58 -0300

    CRUD done!

--- src/main/java/com/demo/myinvest/controller/InversionController.java
+++ src/main/java/com/demo/myinvest/controller/InversionController.java
@@ -1,7 +1,46 @@
 package com.demo.myinvest.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.demo.myinvest.model.Inversion;
+import com.demo.myinvest.service.InversionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class InversionController {
+    @Autowired
+    private InversionService inversionServ;
+
+    /* EndPoints */
+
+    @GetMapping("/inversiones/traertodo")
+    public List<Inversion> getInversiones(){
+        return inversionServ.getInversiones();
+    }
+
+    @PostMapping("/inversiones/crear")
+    public String saveInversion(@RequestBody Inversion inversion){
+        inversionServ.saveInversion(inversion);
+        return "Guardado Correctamente!";
+    }
+
+    @DeleteMapping("/inversiones/borrar/{id}")
+    public String deleteInversion(@RequestParam Long id){
+        inversionServ.deleteInversion(id);
+        return "Inversion Eliminada!";
+    }
+
+    @GetMapping("/inversiones/traer/{id}")
+    public Inversion findInversion(@RequestParam Long id){
+        return inversionServ.findInversion(id);
+    }
+
+    @PutMapping("/inversiones/edit")
+    public String editInversion(@RequestBody Inversion inversion){
+        inversionServ.editInversion(inversion);
+        return "Activo Editado Correctamente!";
+    }
+
+    //Other methods...
 }
